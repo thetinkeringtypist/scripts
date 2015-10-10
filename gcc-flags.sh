9c0d7c05d6d6e88486be912dced3639a4ae06c4d
Evan Bezeredi <bezeredi.dev@gmail.com>
2015-10-10 19:57:27 -0400

    Update

--- gcc-flags.sh
+++ gcc-flags.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 #! Lists the command line flags that are native to your machine.
+#
+#  NOTE: This assumes that you are using gcc 4.6 or later.
 
 gcc -### -E - -march=native 2>&1 | sed -r '/cc1/!d;s/(")|(^.* - )//g'
 
