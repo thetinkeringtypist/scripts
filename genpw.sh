f43f51e2f8e05a8ada9a3f0e74233539a8a5f374
Evan Bezeredi <bezeredi.dev@gmail.com>
2015-10-25 10:48:11 -0400

    genpw.sh: Added variable length help options

--- genpw.sh
+++ genpw.sh
@@ -9,6 +9,19 @@ UPPERCASE=A-Z	#! Upprecase letters allowed
 LOWERCASE=a-z	#! Lowercase letters allowed
 SPECIAL=_#!		#! Special characters allowed
 
+
+#! Help
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+	PROGNAME=`basename "$0"`
+	echo "usage: $PROGNAME [length]"
+	exit
+
+#! Check the length of the password
+elif [ "$1" -eq "$1" ] 2>/dev/null; then
+	LENGTH="$1"
+fi
+
+
 < /dev/urandom tr -dc ${UPPERCASE}${LOWERCASE}${NUMBER}${SPECIAL} \
 	| head --bytes=${LENGTH};echo;
 
