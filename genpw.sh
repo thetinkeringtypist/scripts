70c6dfc06d1cf3c47ac5df933d8773b88cd758ac
Evan Bezeredi <bezeredi.dev@gmail.com>
2015-11-21 14:59:14 -0500

    genpw.sh: added ability to remove special characters

--- genpw.sh
+++ genpw.sh
@@ -3,21 +3,27 @@
 #! This script does not gaurantee that a character from each category is
 #	present in the generated password
 
-LENGTH=30		#! Should be the actual length minus one
-NUMBER=0-9		#! Numbers allowed
-UPPERCASE=A-Z	#! Upprecase letters allowed
-LOWERCASE=a-z	#! Lowercase letters allowed
-SPECIAL=_#!		#! Special characters allowed
+LENGTH=32        #! Should be the actual length minus one
+NUMBER=0-9       #! Numbers allowed
+UPPERCASE=A-Z    #! Upprecase letters allowed
+LOWERCASE=a-z    #! Lowercase letters allowed
+SPECIAL="_#!&^$" #! Special characters allowed
 
 
 #! Help
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
 	PROGNAME=`basename "$0"`
-	echo "usage: $PROGNAME [length]"
+	echo "usage: $PROGNAME [--no-special] [num-chars]"
 	exit
 
+#! Check the use of special characters
+elif [[ "$1" == "--no-special" ]]; then
+	SPECIAL=""
+	shift
+fi
+
 #! Check the length of the password
-elif [ "$1" -eq "$1" ] 2>/dev/null; then
+if [ "$1" -eq "$1" ] 2>/dev/null; then
 	LENGTH="$1"
 fi
 
