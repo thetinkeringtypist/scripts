3f92754c763fd3b4f95b871dbd3dd6c6743aef19
Evan Bezeredi <bezeredi.dev@gmail.com>
2015-11-22 13:22:37 -0500

    Documentation ============= Added documentation to the top of the script. Also prevented issues with negative length passwords.

--- genpw.sh
+++ genpw.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
-
+#
 #! This script does not gaurantee that a character from each category is
 #	present in the generated password
+#
+#  NOTE: This script requires at least one non-empty character set to produce
+#        a password. It also requires n to be greater than 0.
+#  
+#  OPTIONS
+#     --help | -h
+#        Displays usage information
+#
+#     --no-special
+#        Remove the character set for special characters
+#
+#     --no-number
+#        Removes the character set for numbers
+#
+#     --no-upper
+#        Removes the character set for uppercase characters
+#
+#     --no-lower
+#        Removes the character set for lowercase numbers
+#
+#     n
+#        The number of characters to produce. Must be greater than 0.
+#
+#     --
+#        Designates the end of options to be parsed
+
 
 LENGTH=64        #! Should be the actual length minus one
 NUMBER="0-9"     #! Numbers allowed
@@ -16,7 +42,7 @@ while [[ "$1" != "" ]]; do
 	case "$1" in
 		"--help" | "-h")
 			echo "Usage: $PROGNAME [--no-special] [--no-number] [--no-upper]" \
-			     "[--no-lower] [--] [n]"
+			     "[--no-lower] [n] [--]"
 			exit;;
 		"--no-special") SPECIAL="";   shift;;
 		"--no-number")  NUMBER="";    shift;;
@@ -26,6 +52,10 @@ while [[ "$1" != "" ]]; do
 		*)
 			#! Check the length
 			if [ "$1" -eq "$1" ] 2>/dev/null; then
+				if [ "$1" -le 0 ]; then
+					echo "$PROGNAME: n must be greater than 0. Exit."
+					exit
+				fi
 				LENGTH="$1"
 				shift
 				continue
