5ccce3f49799648430c060ac0d569e2a0c7280e9
Evan Bezeredi <bezeredi.dev@gmail.com>
2015-11-22 13:11:45 -0500

    More Options, Robustness

--- genpw.sh
+++ genpw.sh
@@ -3,31 +3,51 @@
 #! This script does not gaurantee that a character from each category is
 #	present in the generated password
 
-LENGTH=32        #! Should be the actual length minus one
-NUMBER=0-9       #! Numbers allowed
-UPPERCASE=A-Z    #! Upprecase letters allowed
-LOWERCASE=a-z    #! Lowercase letters allowed
+LENGTH=64        #! Should be the actual length minus one
+NUMBER="0-9"     #! Numbers allowed
+UPPERCASE="A-Z"  #! Upprecase letters allowed
+LOWERCASE="a-z"  #! Lowercase letters allowed
 SPECIAL="_#!&^$" #! Special characters allowed
+PROGNAME=`basename "$0"`
 
 
-#! Help
-if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-	PROGNAME=`basename "$0"`
-	echo "usage: $PROGNAME [--no-special] [num-chars]"
+#! Parse options
+while [[ "$1" != "" ]]; do
+	case "$1" in
+		"--help" | "-h")
+			echo "Usage: $PROGNAME [--no-special] [--no-number] [--no-upper]" \
+			     "[--no-lower] [--] [n]"
+			exit;;
+		"--no-special") SPECIAL="";   shift;;
+		"--no-number")  NUMBER="";    shift;;
+		"--no-upper")   UPPERCASE=""; shift;;
+		"--no-lower")   LOWERCASE=""; shift;;
+		"--")                         shift; break;;
+		*)
+			#! Check the length
+			if [ "$1" -eq "$1" ] 2>/dev/null; then
+				LENGTH="$1"
+				shift
+				continue
+			fi
+
+			echo "$PROGNAME: unrecognized option: $1. Exit."
+			exit;;
+	esac
+done
+
+
+#! Check character sets
+if [[ "$SPECIAL" == ""   &&
+		"$NUMBER" == ""    &&
+		"$UPPERCASE" == "" &&
+		"$LOWERCASE" == "" ]]; then
+	echo "$PROGNAME: requires at least one non-empty character set. Exit."
 	exit
-
-#! Check the use of special characters
-elif [[ "$1" == "--no-special" ]]; then
-	SPECIAL=""
-	shift
-fi
-
-#! Check the length of the password
-if [ "$1" -eq "$1" ] 2>/dev/null; then
-	LENGTH="$1"
 fi
 
 
+#! Generate the password
 < /dev/urandom tr -dc ${UPPERCASE}${LOWERCASE}${NUMBER}${SPECIAL} \
 	| head --bytes=${LENGTH};echo;
 
